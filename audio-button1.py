791751876615ce415d2e9231a8d858416c68d115
benchenapa94 <65704260+benchenapa94@users.noreply.github.com>
2020-05-22 20:33:12 +0100

    Update audio-button1.py

--- audio-button1.py
+++ audio-button1.py
@@ -24,16 +24,16 @@ global playProcess
     if not button1.value:
 
 os.system('dbuscontrol.sh pause')
-sleep(3)
+time.sleep(3)
 os.system('omxplayer -o local bp.mp3 &')
 print("planted")
-sleep(3)
+time.sleep(3)
 print("siren started")
 os.system('omxplayer -o local bomb.mp3 &')
 
     if not button2.value:
 os.system('dbuscontrol.sh pause')
-sleep(3)
+time.sleep(3)
         os.system('omxplayer -o local bd.mp3 &')
 
     time.sleep(.25)
