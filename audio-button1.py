3ff492573e9aca00859add9b9e32b95c9c84bbe9
benchenapa94 <65704260+benchenapa94@users.noreply.github.com>
2020-05-22 20:35:36 +0100

    Update audio-button1.py

--- audio-button1.py
+++ audio-button1.py
@@ -18,12 +18,12 @@ button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
 while True:
 global playProcess
-     omxplayer -o local <file>
+    # omxplayer -o local <file>
     # omxplayer -o hdmi <file>
     # omxplayer -o both <file>
     if not button1.value:
 
-os.system('dbuscontrol.sh pause')
+#os.system('dbuscontrol.sh pause')
 time.sleep(3)
 os.system('omxplayer -o local bp.mp3 &')
 print("planted")
@@ -32,7 +32,7 @@ print("siren started")
 os.system('omxplayer -o local bomb.mp3 &')
 
     if not button2.value:
-os.system('dbuscontrol.sh pause')
+#os.system('dbuscontrol.sh pause')
 time.sleep(3)
         os.system('omxplayer -o local bd.mp3 &')
 
