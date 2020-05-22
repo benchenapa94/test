82dbd898ede9a2856a94ce5991b473ea4ce7a071
benchenapa94 <65704260+benchenapa94@users.noreply.github.com>
2020-05-22 20:30:43 +0100

    Add files via upload

---
+++ audio-button.py
@@ -0,0 +1,39 @@
+# This script requires a Raspberry Pi 2, 3 or Zero. Circuit Python must
+# be installed and it is strongly recommended that you use the latest
+# release of Raspbian.
+
+import time
+import os
+import board
+import digitalio
+
+print("press a button!")
+
+button1 = digitalio.DigitalInOut(board.D23)
+button1.direction = digitalio.Direction.INPUT
+button1.pull = digitalio.Pull.UP
+
+button2 = digitalio.DigitalInOut(board.D24)
+button2.direction = digitalio.Direction.INPUT
+button2.pull = digitalio.Pull.UP
+while True:
+global playProcess
+    # omxplayer -o local <file>
+    # omxplayer -o hdmi <file>
+    # omxplayer -o both <file>
+    if not button1.value:
+
+os.system('dbuscontrol.sh pause')
+sleep(3)
+os.system('omxplayer -o local bp.mp3 &')
+print("planted")
+sleep(3)
+print("siren started")
+os.system('omxplayer -o local bomb.mp3 &')
+
+    if not button2.value:
+os.system('dbuscontrol.sh pause')
+sleep(3)
+        os.system('omxplayer -o local bd.mp3 &')
+
+    time.sleep(.25)
