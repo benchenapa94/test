cde5351ac55f3a70343867f9b7b047c9fdd7f58a
benchenapa94 <65704260+benchenapa94@users.noreply.github.com>
2020-05-22 21:17:45 +0100

    Update audio-button1.py

--- audio-button1.py
+++ audio-button1.py
@@ -14,6 +14,12 @@ VIDEO_1_PATH = "bp.mp3"
 VIDEO_2_PATH = "bd.mp3"
 VIDEO_3_PATH = "bomb.mp3"
 player_log = logging.getLogger("Player 1")
+player = OMXPlayer(VIDEO_1_PATH,
+dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+player2 = OMXPlayer(VIDEO_2_PATH,
+dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+player3 = OMXPlayer(VIDEO_3_PATH,
+dbus_name='org.mpris.MediaPlayer2.omxplayer1')
 print("press a button!")
 
 button1 = digitalio.DigitalInOut(board.D23)
@@ -23,27 +29,21 @@ button1.pull = digitalio.Pull.UP
 button2 = digitalio.DigitalInOut(board.D24)
 button2.direction = digitalio.Direction.INPUT
 button2.pull = digitalio.Pull.UP
+
 while True:
     # omxplayer -o local <file>
     # omxplayer -o hdmi <file>
     # omxplayer -o both <file>
     if not button1.value:
 
-player = OMXPlayer(VIDEO_1_PATH,
-dbus_name='org.mpris.MediaPlayer2.omxplayer1')
-player2 = OMXPlayer(VIDEO_3_PATH,
-dbus_name='org.mpris.MediaPlayer2.omxplayer1')
+
 print("planted")
 time.sleep(3)
 print("siren started")
 player.playEvent += lambda _: player_log.info("Play")
-player2.playEvent += lambda _: player_log.info("play")
+player3.playEvent += lambda _: player_log.info("play")
   
     if not button2.value:
-        player = OMXPlayer(VIDEO_1_PATH,
-dbus_name='org.mpris.MediaPlayer2.omxplayer1')
-        player = OMXPlayer(VIDEO__PATH,
-dbus_name='org.mpris.MediaPlayer2.omxplayer1')
-player.playEvent += lambda _: player_log.info("stop")
-player.playEvent += lambda _: player_log.info("stop")
+player3.playEvent += lambda _: player_log.info("stop")
+player2.playEvent += lambda _: player_log.info("play")
     time.sleep(.25)
